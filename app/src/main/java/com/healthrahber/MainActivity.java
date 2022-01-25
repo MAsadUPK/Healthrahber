925c3f14719cff312221924bb559c790032eb6b0
Muhammad Asad <masadupk@gmail.com>
2022-01-25 10:55:04 +0500

    Initial commit

---
+++ app/src/main/java/com/healthrahber/MainActivity.java
@@ -0,0 +1,41 @@
+package com.healthrahber;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Open Login Activity
+
+        Button loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //Open Register Activity
+
+        Button registerButton = findViewById(R.id.register_button);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
